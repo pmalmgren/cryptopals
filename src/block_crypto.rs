6474e5dcc8c6a55853e603f7ba59b5d755450489
Peter Malmgren <ptmalmgren@gmail.com>
2021-11-01 08:16:28 -0400

    almost have challenge14 working

--- src/block_crypto.rs
+++ src/block_crypto.rs
@@ -1,7 +1,7 @@
 use super::CliError;
 use crate::base64;
 use crate::xor::fixed_xor;
-use openssl::symm::{decrypt, encrypt, Cipher, Crypter, Mode};
+use openssl::symm::{Cipher, Crypter, Mode};
 use rand::Rng;
 use std::collections::HashMap;
 use std::error::Error;
@@ -190,20 +190,27 @@ impl UserProfile {
             .to_vec()
     }
 
+    #[allow(dead_code)]
     fn deserialize(input: &str) -> Result<Self, Box<dyn Error>> {
         let parts: Vec<&str> = input.split('&').collect();
+
         if parts.len() != 3 {
             return Err(CliError("invalid data".into()).into());
         }
 
-        if !parts[0].contains("email=") || !parts[1].contains("uid=") || !parts[2].contains("role=")
-        {
-            return Err(CliError("invalid data".into()).into());
-        }
+        let mut email = String::from("");
+        let mut uid = 0;
+        let mut role = String::from("");
 
-        let email = parts[0].replace("email=", "");
-        let uid = parts[1].replace("uid=", "").parse()?;
-        let role = parts[2].replace("role=", "");
+        for part in parts {
+            if part.contains("email=") {
+                email = part.replace("email=", "");
+            } else if part.contains("uid=") {
+                uid = part.replace("uid=", "").parse()?;
+            } else if part.contains("role=") {
+                role = part.replace("role=", "");
+            }
+        }
 
         Ok(Self { email, uid, role })
     }
@@ -218,7 +225,7 @@ impl UserProfile {
     fn get_key() -> Result<Vec<u8>, Box<dyn Error>> {
         match std::fs::read("profile_key.txt") {
             Ok(k) => Ok(k),
-            Err(e) => {
+            Err(_e) => {
                 let mut rng = rand::thread_rng();
                 let k = generate_random_bytes(16, &mut rng);
                 std::fs::write("profile_key.txt", &k)?;
@@ -232,10 +239,9 @@ impl UserProfile {
         Ok(output)
     }
 
+    #[allow(dead_code)]
     pub(crate) fn decrypt(input: &[u8]) -> Result<Self, Box<dyn Error>> {
         let decrypted = decrypt_ecb(&UserProfile::get_key()?, input)?;
-        println!("input = {:?}", input);
-        println!("decrypted = {:?}", decrypted);
         UserProfile::deserialize(std::str::from_utf8(&decrypted)?)
     }
 }
@@ -243,22 +249,25 @@ impl UserProfile {
 pub struct Oracle {
     key: Vec<u8>,
     input: Vec<u8>,
+    prefix: Vec<u8>
 }
 
 impl Oracle {
     pub fn new() -> Result<Self, Box<dyn Error>> {
+        let mut rng = rand::thread_rng();
         let key = match std::fs::read("key.txt") {
             Ok(k) => k,
             Err(_) => {
-                let mut rng = rand::thread_rng();
                 let k = generate_random_bytes(16, &mut rng);
                 std::fs::write("key.txt", &k)?;
                 k
             }
         };
+        let prefix: Vec<u8> = generate_random_bytes(rng.gen_range(1..64), &mut rng);
         let input: String = std::fs::read_to_string("12.txt").unwrap().replace('\n', "");
         let to_crack = base64::base64_to_bytes(&input).expect("should decode");
         Ok(Self {
+            prefix,
             key,
             input: to_crack,
         })
@@ -269,32 +278,49 @@ impl Oracle {
         bytes.extend_from_slice(&self.input);
         encrypt_ecb(&self.key, &bytes)
     }
+
+    pub fn encrypt_prefix(&self, bytes: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+        let mut prefix = self.prefix.clone();
+        prefix.extend_from_slice(&bytes); 
+        prefix.extend_from_slice(&self.input);
+        encrypt_ecb(&self.key, &prefix)
+    }
 }
 
 /// Given a block size, some pad, and an oracle, create a lookup that will
 /// match encrypted byte slices against bytes
-fn create_lookup(
+fn create_lookup<F>(
     pad_bytes: &[u8],
     block_size: usize,
-    oracle: &Oracle,
-) -> Result<HashMap<Vec<u8>, u8>, Box<dyn Error>> {
+    block_num: usize,
+    oracle: &F,
+) -> Result<HashMap<Vec<u8>, u8>, Box<dyn Error>>
+where
+    F: Fn(&[u8]) -> Result<Vec<u8>, Box<dyn Error>>,
+{
     let mut map: HashMap<Vec<u8>, u8> = HashMap::with_capacity(256);
     for byte in 0u8..255 {
         let mut padded_input = pad_bytes.to_vec();
         padded_input.push(byte);
-        let encrypted = oracle.encrypt(&padded_input)?;
-        let first_block = encrypted[0..block_size].to_vec();
+        let encrypted = oracle(&padded_input)?;
+        let start = block_num * block_size;
+        let end = std::cmp::min(start + block_size, encrypted.len() - 1);
+        let first_block = encrypted[start..end].to_vec();
         map.insert(first_block, byte);
     }
     Ok(map)
 }
 
-pub fn decrypt_block(
+pub fn decrypt_block<F>(
     block_num: usize,
     block_size: usize,
-    oracle: &Oracle,
+    offset: usize,
     pad: &[u8],
-) -> Result<Vec<u8>, Vec<u8>> {
+    encrypt_fn: F,
+) -> Result<Vec<u8>, Vec<u8>>
+where
+    F: Fn(&[u8]) -> Result<Vec<u8>, Box<dyn Error>>,
+{
     // bytes = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     // block size = 3
     // append block size minus 1
@@ -322,10 +348,9 @@ pub fn decrypt_block(
         // what we send to the oracle to push bytes over
         let encryption_pad: Vec<u8> = (0..pad_size).map(|_| 'A' as u8).collect();
 
-        let lookup = create_lookup(&pad, block_size, &oracle).expect("should create the oracle");
-        let encrypted = oracle
-            .encrypt(&encryption_pad)
-            .expect("should encrypt the text");
+        let lookup =
+            create_lookup(&pad, block_size, offset, &encrypt_fn).expect("should create the lookup");
+        let encrypted = encrypt_fn(&encryption_pad).expect("should encrypt the text");
 
         let start = block_num * block_size;
         let end = std::cmp::min(start + block_size, encrypted.len() - 1);
@@ -340,6 +365,7 @@ pub fn decrypt_block(
             return Err(plaintext);
         }
         let first_block = encrypted[start..end].to_vec();
+        println!("first_block = {:?}", first_block);
         let first_char = lookup.get(&first_block);
         if let Some(ch) = first_char {
             plaintext.push(*ch);
@@ -354,11 +380,14 @@ pub fn decrypt_block(
     return Ok(plaintext);
 }
 
-pub fn discover_blocksize(oracle: &Oracle) -> Result<usize, Box<dyn std::error::Error>> {
-    let encrypted_len = oracle.encrypt("".as_bytes())?.len();
+pub fn discover_blocksize<F>(oracle: &F) -> Result<usize, Box<dyn std::error::Error>>
+where
+    F: Fn(&[u8]) -> Result<Vec<u8>, Box<dyn Error>>,
+{
+    let encrypted_len = oracle("".as_bytes())?.len();
     for idx in 1..32 {
         let guess: Vec<u8> = (0..idx).map(|_i| 'a' as u8).collect();
-        let encrypted = oracle.encrypt(&guess)?;
+        let encrypted = oracle(&guess)?;
         let diff = encrypted.len() - encrypted_len;
         if diff > 0 {
             return Ok(diff);
--- src/main.rs
+++ src/main.rs
@@ -339,7 +339,7 @@ fn challenge11() -> Result<(), Box<dyn Error>> {
 fn challenge12() -> Result<(), Box<dyn Error>> {
     let oracle = block_crypto::Oracle::new()?;
 
-    let block_size = block_crypto::discover_blocksize(&oracle)?;
+    let block_size = block_crypto::discover_blocksize(&|bytes: &[u8]| oracle.encrypt(bytes))?;
     println!("Discovered block size = {}", block_size);
 
     let input = yellow_submarine_10_times();
@@ -350,7 +350,9 @@ fn challenge12() -> Result<(), Box<dyn Error>> {
     let mut pad: Vec<u8> = (0..block_size - 1).map(|_| 'A' as u8).collect();
 
     for (idx, _) in encrypted.chunks(block_size).enumerate() {
-        let result = block_crypto::decrypt_block(idx, block_size, &oracle, &pad);
+        let result = block_crypto::decrypt_block(idx, block_size, 0, &pad, &|bytes: &[u8]| {
+            oracle.encrypt(bytes)
+        });
         match result {
             Ok(bytes) => {
                 pad = bytes[1..].to_vec();
@@ -368,6 +370,192 @@ fn challenge12() -> Result<(), Box<dyn Error>> {
     println!("Decrypted = {}", plaintext);
     Ok(())
 }
+
+fn challenge13() -> Result<(), Box<dyn Error>> {
+    // blocks: ["email=abc@ab.com"], ["admin\x0B..."], ...
+    let email = "abc@ab.comadmin\x0B\x0B\x0B\x0B\x0B\x0B\x0B\x0B\x0B\x0B\x0B";
+    // blocks: ["email=1234567@a.", "com&uid=10&role=", "user"]
+    let template = "1234567@a.com";
+    let mut encrypted_template = block_crypto::UserProfile::profile_for(template).encrypt()?[0..32].to_vec();
+    let admin_block = &block_crypto::UserProfile::profile_for(email).encrypt()?[16..32];
+    encrypted_template.extend_from_slice(admin_block);
+
+    let admin_profile = block_crypto::UserProfile::decrypt(&encrypted_template)?;
+    println!("{:?}", admin_profile);
+
+    Ok(())
+}
+
+mod challenge14 {
+    use std::error::Error;
+    use crate::error::CliError;
+
+    #[derive(Debug)]
+    pub struct RandomPad {
+        pub controlled_block: usize,
+        pub pad_bytes: Vec<u8>
+    }
+
+    /// Given an oracle which pads random bytes at the beginning, find the pad needed
+    /// to give the attacker control over the next block, which is the start of the
+    /// ciphertext.
+    /// Algorithm (find suspect block):
+    /// 1. Get all encrypted blocks
+    /// 2. Encrypt a single attacker controlled byte
+    /// 3. Find the first block in the new cipher text different than the old
+    ///
+    /// After we know which block to start with
+    /// 1. Find the suspect block index
+    /// 2. Loop: Add a byte at a time
+    /// 3. Find the first block in the new cipher text different than the old
+    /// 4. Stop when it changes
+    pub fn detect_random_pad<F>(block_size: usize, oracle: &F) -> Result<RandomPad, Box<dyn Error>>
+        where F: Fn(&[u8]) -> Result<Vec<u8>, Box<dyn Error>>
+    {
+        let encrypted_blank = oracle("".as_bytes())?; 
+        let encrypted_one = oracle("a".as_bytes())?; 
+        let mut suspect_chunk = -1;
+        let mut last_chunk = vec![0; block_size];
+        for (idx, (chunk, chunk2)) in encrypted_blank.chunks(block_size).zip(encrypted_one.chunks(block_size)).enumerate() {
+            if chunk != chunk2 {
+                suspect_chunk = idx as i32;
+                last_chunk = chunk.to_vec();
+                break;
+            }
+        }
+
+        if suspect_chunk == -1 {
+            return Err(CliError("Error finding where attacker controlled bytes start".into()).into());
+        }
+
+        for nbytes in 1..(block_size + 2) {
+            let pad_bytes: Vec<u8> = (0..nbytes)
+                .map(|_| 'A' as u8)
+                .collect();
+            let encrypted = oracle(&pad_bytes)?;
+            let controlled_block = suspect_chunk as usize;
+            let start = controlled_block * block_size;
+            let end = start + block_size;
+            let chunk = &encrypted[start..end]; 
+            if chunk == &last_chunk {
+                let pad_bytes = pad_bytes[0..nbytes-1].to_vec();
+                return Ok(RandomPad { controlled_block: controlled_block + 1, pad_bytes });
+            }
+            last_chunk = chunk.to_vec();
+        }
+
+        return Err(CliError("Didn't find random bytes".into()).into());
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        fn fake_oracle(pad: usize, b: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+            let fake_message = "YELLOW SUBMARINEYELLOW SUBMARINE".as_bytes();
+            let mut fake_ciphertext: Vec<u8> = (0..pad)
+                .map(|_| 'R' as u8)
+                .collect();
+            fake_ciphertext.extend_from_slice(b);
+            fake_ciphertext.extend_from_slice(&fake_message);
+            let rem = fake_ciphertext.len() % 16;
+            if rem > 0 {
+                let bytes_to_append = 16 - rem;
+                let bytes: Vec<u8> = (0..bytes_to_append)
+                    .map(|_| 'P' as u8)
+                    .collect();
+                fake_ciphertext.extend_from_slice(&bytes);
+            }
+            assert!(fake_ciphertext.len() % 16 == 0);
+            Ok(fake_ciphertext)
+        }
+
+        #[test]
+        fn test_detect_random_pad() {
+            let res = detect_random_pad(16, &|b: &[u8]| fake_oracle(5, b)).expect("should find where attacker controlled bytes start");
+            assert_eq!(res.controlled_block, 1);
+            assert_eq!(res.pad_bytes.len(), 11);
+            let fake_encrypted_bytes = fake_oracle(5, &res.pad_bytes).expect("should fake encrypt the bytes");
+            let str0 = std::str::from_utf8(&fake_encrypted_bytes[0..16]).expect("should produce a string");
+            let str1 = std::str::from_utf8(&fake_encrypted_bytes[16..32]).expect("should produce a string");
+            let str2 = std::str::from_utf8(&fake_encrypted_bytes[32..48]).expect("should produce a string");
+            assert_eq!(str0, "RRRRRAAAAAAAAAAA");
+            assert_eq!(str1, "YELLOW SUBMARINE");
+            assert_eq!(str2, "YELLOW SUBMARINE");
+        }
+
+        #[test]
+        fn test_detect_random_pad_one() {
+            let res = detect_random_pad(16, &|b: &[u8]| fake_oracle(15, b)).expect("should find where attacker controlled bytes start");
+            assert_eq!(res.controlled_block, 1);
+            assert_eq!(res.pad_bytes.len(), 1);
+            let fake_encrypted_bytes = fake_oracle(15, &res.pad_bytes).expect("should fake encrypt the bytes");
+            let str0 = std::str::from_utf8(&fake_encrypted_bytes[0..16]).expect("should produce a string");
+            let str1 = std::str::from_utf8(&fake_encrypted_bytes[16..32]).expect("should produce a string");
+            let str2 = std::str::from_utf8(&fake_encrypted_bytes[32..48]).expect("should produce a string");
+            assert_eq!(str0, "RRRRRRRRRRRRRRRA");
+            assert_eq!(str1, "YELLOW SUBMARINE");
+            assert_eq!(str2, "YELLOW SUBMARINE");
+        }
+
+        #[test]
+        fn test_detect_random_pad_zero() {
+            let res = detect_random_pad(16, &|b: &[u8]| fake_oracle(0, b)).expect("should find where attacker controlled bytes start");
+            assert_eq!(res.controlled_block, 1);
+            assert_eq!(res.pad_bytes.len(), 16);
+            let fake_encrypted_bytes = fake_oracle(0, &res.pad_bytes).expect("should fake encrypt the bytes");
+            let str0 = std::str::from_utf8(&fake_encrypted_bytes[0..16]).expect("should produce a string");
+            let str1 = std::str::from_utf8(&fake_encrypted_bytes[16..32]).expect("should produce a string");
+            let str2 = std::str::from_utf8(&fake_encrypted_bytes[32..48]).expect("should produce a string");
+            assert_eq!(str0, "AAAAAAAAAAAAAAAA");
+            assert_eq!(str1, "YELLOW SUBMARINE");
+            assert_eq!(str2, "YELLOW SUBMARINE");
+        }
+
+        #[test]
+        fn test_detect_random_pad_sixteen() {
+            let res = detect_random_pad(16, &|b: &[u8]| fake_oracle(16, b)).expect("should find where attacker controlled bytes start");
+            assert_eq!(res.controlled_block, 2);
+            assert_eq!(res.pad_bytes.len(), 16);
+            let fake_encrypted_bytes = fake_oracle(16, &res.pad_bytes).expect("should fake encrypt the bytes");
+            let str0 = std::str::from_utf8(&fake_encrypted_bytes[0..16]).expect("should produce a string");
+            let str1 = std::str::from_utf8(&fake_encrypted_bytes[16..32]).expect("should produce a string");
+            let str2 = std::str::from_utf8(&fake_encrypted_bytes[32..48]).expect("should produce a string");
+            let str3 = std::str::from_utf8(&fake_encrypted_bytes[48..64]).expect("should produce a string");
+            assert_eq!(str0, "RRRRRRRRRRRRRRRR");
+            assert_eq!(str1, "AAAAAAAAAAAAAAAA");
+            assert_eq!(str2, "YELLOW SUBMARINE");
+            assert_eq!(str3, "YELLOW SUBMARINE");
+        }
+    }
+}
+
+fn challenge14() -> Result<(), Box<dyn Error>> {
+    let oracle = block_crypto::Oracle::new()?;
+    let block_size = block_crypto::discover_blocksize(&|bytes: &[u8]| oracle.encrypt_prefix(bytes))?;
+    println!("Discovered block size = {}", block_size);
+
+    let random_pad = challenge14::detect_random_pad(block_size, &|bytes: &[u8]| oracle.encrypt_prefix(bytes))?;
+    println!("Discovered random bytes = {:?}", random_pad);
+
+    let mut _plaintext: String = "".to_string();
+    let pad: Vec<u8> = (0..block_size - 1).map(|_| 'A' as u8).collect();
+    // the input looks like this:
+    // [RB, RB1, RB2, RB3, RB4, P1, P2, P3] [B1, B2, B3, B4, B5, B6, B7, B8]
+    // where RB = random byte, P = our pad bytes
+    // attack 1:
+    // [RB, RB1, RB2, RB3, RB4, P1, P2, P3] [A, A, A, A, A, A, A, B1] [B2, B3, B4, B5, B6, B7, B8, B9]
+    // attack 2:
+    // [RB, RB1, RB2, RB3, RB4, P1, P2, P3] [A, A, A, A, A, A, A, B1] [B2, B3, B4, B5, B6, B7, B8, B9]
+    let decrypted = block_crypto::decrypt_block(random_pad.controlled_block, block_size, random_pad.controlled_block, &pad, &|bytes: &[u8]| {
+        let mut to_encrypt = random_pad.pad_bytes.clone();
+        to_encrypt.extend_from_slice(bytes);
+        oracle.encrypt_prefix(&to_encrypt)
+    });
+
+    Ok(())
+}
+
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = args().collect();
@@ -391,6 +579,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         "challenge10" => challenge10(),
         "challenge11" => challenge11(),
         "challenge12" => challenge12(),
+        "challenge13" => challenge13(),
+        "challenge14" => challenge14(),
         cmd => {
             eprintln!("Unknown subcommand {}", cmd);
             return Err(CliError("Unknown command".to_string()).into());
